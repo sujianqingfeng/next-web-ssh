6ecbaa3832b1d233e9542a5b1fab77311c63b532
hens <121116111@qq.com>
2024-01-16 16:33:13 +0800

    chore: config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: 'node-loader'
+    })
+    return config
+  }
 }
 
 module.exports = nextConfig
